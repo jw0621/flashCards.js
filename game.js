9f9ad7b18005880d1241302dd8b10928f9387668
Tom <tomw0621@gmail.com>
2018-06-14 22:38:02 -0700

    added pseudocode for game.js

---
+++ game.js
@@ -0,0 +1,11 @@
+// create array of basic cards
+// create array of cloze cards
+// var score
+// now game code
+// initialize function: set score equal to 0 and then inquirer for basic card questions (easy) or cloze card questions (hard)
+// basic card question function: for loop through basic card array
+// inquirer w/ message using basicCard.front
+// if input == basicCard.back, console.log("You are correct!") and score++;
+// else console.log("Wrong! The correct answer was: "+basicCard.back+"!");
+// once i reaches end of array, show final score, then return to initialze function;
+// cloze card game is similar, except with .text and .cloze instead of .front and .back
