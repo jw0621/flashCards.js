50d04a0db80e105cce9527b3f4756962354c6f42
Tom <tomw0621@gmail.com>
2018-06-14 22:03:00 -0700

    Finished BasicCard and ClozeCard javascripts

--- BasicCard.js
+++ BasicCard.js
@@ -1,1 +1,7 @@
+// creates a BasicCard object w/ frong and back attributes
+var BasicCard = function(front, back) {
+    this.front=front;
+    this.back = back;
+};
+// export the module
 module.exports = BasicCard
---
+++ ClozeCard.js
@@ -0,0 +1,19 @@
+var BasicCard = require('./BasicCard.js');
+var ClozeCard = function (text, cloze) {
+    if (this instanceof ClozeCard){
+        this.fullText = text;
+        this.cloze = cloze;
+        this.partial = function () {
+            if (this.fullText.includes(this.cloze)) {
+                return this.fullText.replace(this.cloze, '______');
+            } else {
+                var failedCloze = 'Oops! The full text: "' + this.fulltext + '" does not include: "' + this.cloze + '!';
+                return failedCloze;
+            }
+        };
+    }
+    else {
+        return new ClozeCard(text, cloze)
+    }
+}
+module.exports = ClozeCard
